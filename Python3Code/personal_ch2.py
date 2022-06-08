6bd412dedc1a36f4eb565bfa5c68deabc5a7b6e5
dariusbarsony <dariusbarsony@gmail.com>
2022-06-08 12:01:06 +0200

    assignment and intermediate datafiles generated

---
+++ Python3Code/personal_ch2.py
@@ -0,0 +1,92 @@
+##############################################################
+#                                                            #
+#    Mark Hoogendoorn and Burkhardt Funk (2017)              #
+#    Machine Learning for the Quantified Self                #
+#    Springer                                                #
+#    Chapter 2                                               #
+#                                                            #
+##############################################################
+
+# Import the relevant classes.
+from Chapter2.CreateDataset import CreateDataset
+from util.VisualizeDataset import VisualizeDataset
+from util import util
+from pathlib import Path
+import copy
+import os
+import sys
+
+# Chapter 2: Initial exploration of the dataset.
+
+DATASET_PATH = Path('./datasets/data_personal/')
+RESULT_PATH = Path('./personal_data_result/')
+RESULT_FNAME = 'personal_data_chapter2_result.csv'
+
+# Set a granularity (the discrete step size of our time series data). We'll use a course-grained granularity of one
+# instance per minute, and a fine-grained one with four instances per second.
+GRANULARITIES = [60000, 250]
+
+# We can call Path.mkdir(exist_ok=True) to make any required directories if they don't already exist.
+[path.mkdir(exist_ok=True, parents=True) for path in [DATASET_PATH, RESULT_PATH]]
+
+print('Please wait, this will take a while to run!')
+
+datasets = []
+for milliseconds_per_instance in GRANULARITIES:
+    print(f'Creating numerical datasets from files in {DATASET_PATH} using granularity {milliseconds_per_instance}.')
+
+    # Create an initial dataset object with the base directory for our data and a granularity
+    dataset = CreateDataset(DATASET_PATH, milliseconds_per_instance)
+
+    # Add the selected measurements to it.
+
+    # We add the accelerometer data (continuous numerical measurements) of the phone and the smartwatch
+    # and aggregate the values per timestep by averaging the values
+    dataset.add_numerical_dataset('accelerometer_long_darius.csv', 'Time (s)', ['Linear Acceleration x (m/s^2)','Linear Acceleration y (m/s^2)','Linear Acceleration z (m/s^2)', 'Absolute acceleration (m/s^2)'], 'avg', 'acc_phone')
+
+    # We add the gyroscope data (continuous numerical measurements) of the phone and the smartwatch
+    # and aggregate the values per timestep by averaging the values
+    dataset.add_numerical_dataset('gyroscope_long_darius.csv', 'Time (s)', ['Gyroscope x (rad/s)','Gyroscope y (rad/s)','Gyroscope z (rad/s)', 'Absolute (rad/s)'], 'avg', 'gyr_phone')
+
+    # We add the labels provided by the users. These are categorical events that might overlap. We add them
+    # as binary attributes (i.e. add a one to the attribute representing the specific value for the label if it
+    # occurs within an interval).
+
+    # dataset.add_event_dataset('labels.csv', 'label_start', 'label_end', 'label', 'binary')
+
+    # We add the audio and location data and aggregate by averaging
+    dataset.add_numerical_dataset('audio_darius.csv', 'Time (s)', ['Recording (a.u.)'], 'avg', 'audio_')
+    dataset.add_numerical_dataset('location_long_darius.csv', 'Time (s)', ["Latitude (°)","Longitude (°)","Altitude (m)","Altitude WGS84 (m)","Speed (m/s)","Direction (°)","Distance (km)",
+        "Horizontal Accuracy (m)","Vertical Accuracy (m)","Satellites"], 'avg', 'acc_phone')
+
+    # Get the resulting pandas data table
+    dataset = dataset.data_table
+
+    # Plot the data
+    DataViz = VisualizeDataset(__file__)
+
+    # Boxplot
+    DataViz.plot_dataset_boxplot(dataset, ['acc_phone_x','acc_phone_y','acc_phone_z'])
+
+    # Plot all data
+    DataViz.plot_dataset(dataset, ['acc_', 'gyr_', 'audio', 'location', 'label'],
+                                  ['like', 'like', 'like', 'like', 'like', 'like', 'like','like'],
+                                  ['line', 'line', 'line', 'line', 'line', 'line', 'points', 'points'])
+
+    # And print a summary of the dataset.
+    util.print_statistics(dataset)
+    datasets.append(copy.deepcopy(dataset))
+
+    # If needed, we could save the various versions of the dataset we create in the loop with logical filenames:
+    # dataset.to_csv(RESULT_PATH / f'chapter2_result_{milliseconds_per_instance}')
+
+
+# Make a table like the one shown in the book, comparing the two datasets produced.
+util.print_latex_table_statistics_two_datasets(datasets[0], datasets[1])
+
+# Finally, store the last dataset we generated (250 ms).
+dataset.to_csv(RESULT_PATH / RESULT_FNAME)
+
+# Lastly, print a statement to know the code went through
+
+print('The code has run through successfully!')
